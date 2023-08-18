c9367599757b2a93b3d12172d234f170c0912583
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2023-08-18 08:24:36 -0500

    First Committ in GUI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from security_hub_information.utilities import find_name, validate_password, validate_security_question
 
 # Actual Application
-
 titans_name = input("What is your teen titan's name: ").title()
 
 character_info = find_name(titans_name)
