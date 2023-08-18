cd17a5075d54d409df7504c75afab69930039dcb
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2023-08-18 08:20:33 -0500

    Updated the documenation and code.

--- main.py
+++ main.py
@@ -32,6 +32,14 @@ TEEN_TITANS = [
 ]
 
 def find_name(target_name: str) -> dict:
+    """
+    This function will take a string containing a users input for what Titan they want to log in as.
+    If the Titan Name they type is valid, the function should return a dictionary with that information.
+    Else if not found,
+
+    :param target_name: String containing the users input for what teen titans.
+    :return: Dict OR None. If none is returned the character doesn't exist.
+    """
     a = None
     for item in TEEN_TITANS:
         if target_name == item["name"]:
@@ -61,10 +69,13 @@ def validate_security_question(character_data: dict) -> bool:
 
     # Make sure a user doesn't use more than three attempts to enter his/her password.
     while attempts1 < 3:
-        response = input(f"{character_data['security_question']}: ")
-        if response.lower().strip() == character_data['security_answer'].lower():
+        response: str = input(f"{character_data['security_question']}: ")
+        if response == character_data['security_answer']:
             access = True
             break
+        elif response.lower() == character_data['security_answer']:
+            attempts1 += 1
+            print(f"Security question answer was wrong. Watch your capitalization. You have {3 - attempts1} attempt(s) left")
         else:
             attempts1 += 1
             print(f"Security question answer was wrong. You have {3 - attempts1} attempt(s) left")
