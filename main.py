0f821c3350fd2710ecb45ce7c97c46c5c22de314
Kameron Arceneaux <kameronarceneaux05@Gmail.com>
2023-08-18 08:58:32 -0500

    Sample Test to the GUI

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from security_hub_information.utilities import find_name, validate_password, validate_security_question
+import tkinter as tk
+
+root = tk.Tk()
+root.title("TEEN TITANS SECURITY HUB SCREEN")
+root.geometry("650x550+400+100")
+root.resizable(False, False)
+
+
+root.mainloop()
 
 # Actual Application
-titans_name = input("What is your teen titan's name: ").title()
-
-character_info = find_name(titans_name)
+# titans_name = input("What is your teen titan's name: ").title()
+#
+# character_info = find_name(titans_name)
 
 #If the character name is found
-if character_info:
-    # Validate the characters password
-    passInformation = validate_password(character_info)
-
-    if passInformation:
-        # VALIDATE SECURITY QUESTIONS
-        securityCheck = validate_security_question(character_info)
-
-        if securityCheck:
-            print("ACCESS APPROVED")
-        else:
-            print("ACCESS DENIED.")
-
-    else:
-        print("Out of attempts. ACCESS DENIED.")
-
-else:
-    print("Character not found.")
+# if character_info:
+#     # Validate the characters password
+#     passInformation = validate_password(character_info)
+#
+#     if passInformation:
+#         # VALIDATE SECURITY QUESTIONS
+#         securityCheck = validate_security_question(character_info)
+#
+#         if securityCheck:
+#             print("ACCESS APPROVED")
+#         else:
+#             print("ACCESS DENIED.")
+#
+#     else:
+#         print("Out of attempts. ACCESS DENIED.")
+#
+# else:
+#     print("Character not found.")
 
 
